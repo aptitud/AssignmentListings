aae228e827287b629def6976da8acb40b872ed0a
Johannes Gunnback <johannes.gunnback@aptitud.se>
2014-10-13 10:19:06 +0200

    changed port

--- server.js
+++ server.js
@@ -24,9 +24,13 @@ app.use(bodyParser.urlencoded({'extended':'true'})); 			// parse application/x-w
 app.use(bodyParser.json()); 									// parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 app.use(methodOverride());
+app.set('port', (process.env.PORT || 5000));
 
 // listen (start app with node server.js) ======================================
-app.listen(8080);
+app.listen(app.get('port'), function() {
+    console.log("Node app is running at localhost:" + app.get('port'))
+});
+
 console.log("App listening on port 8080");
 
 app.get('/scrape', function(req, res){
